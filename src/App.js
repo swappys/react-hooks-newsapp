c0693c38fedd52ddc547b955ef1f00f8e24fb66e
Swapnil <swapnilvernekar04@gmail.com>
2022-03-14 11:25:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
+  const [loading, setLoading] = useState(false);
+  //fetch news
+
+
+
+
+
+  const fetchNews = () => {
+    setLoading(true);
+    fetch(url)
+      .then(result => result.json())
+
+      .then(data => (setNews(data.hits), setLoading(false)))
+      .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, [url]);
+
+  const handleChange = (e) => {
+
+    setSearchQuery(e.target.value)
+
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+
+  }
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>News</h2>
+      {loading ? <h2>Loading...</h2> : ""}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
+      {news.map((n, i) => (
+        <p key={i}>{n.title}</p>
+      ))}
     </div>
-  );
+
+  )
 }
 
+
+
+
+
+
+
+// const App =() => {
+//   const [count, setCount] = useState(0);
+
+//   useEffect( () => {
+//     document.title= `Clicked ${count} times`
+
+//   });
+
+//   const increment = () =>{
+//     setCount (count+1);
+//   }
+
+//   return(
+//     <div>
+//         <h2>Counter app</h2>
+//         <button onClick={increment}>
+//           Clicked {count} times
+//         </button>
+//       </div>
+//   );
+
+// }
+
+
+
+
+// class App extends Component{
+//   state = {
+//     count:0
+//   };
+
+//   increment=()=>{
+//     this.setState({
+//       count:this.state.count+1
+//     });
+//   };
+
+//   componentDidMount(){
+//     document.title= `Clicked ${this.state.count} times`;
+//   }
+
+//   componentDidUpdate(){
+//     document.title= `Clicked ${this.state.count} times`;
+//   }
+
+
+
+
+
+//   render(){
+
+//     return(
+//       <div>
+//         <h2>Counter app</h2>
+//         <button onClick={this.increment}>
+//           Clicked {this.state.count} times
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 export default App;
